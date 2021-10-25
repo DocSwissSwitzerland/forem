ec1663bf97713307afc8416a4acbcab97962dccd
Erwin Wolff <erwinwolff@hotmail.com>
2021-10-25 03:30:29 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load if Rails.env.test? || Rails.env.development?
+#Dotenv::Railtie.load if Rails.env.test? || Rails.env.development?
 
 module PracticalDeveloper
   class Application < Rails::Application
